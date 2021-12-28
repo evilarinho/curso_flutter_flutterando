572fc566743985ccee69ce84a9dd5253988637e7
edilson@Acer-E5-473 <ed.vilarinho@gmail.com>
2021-12-28 18:17:03 -0300

    atualizado até a aula09

--- ola_mundo/lib/main.dart
+++ ola_mundo/lib/main.dart
@@ -1,42 +1,6 @@
 import 'package:flutter/material.dart';
+import 'app_widget.dart';
 
 main() {
-  runApp(AppWidget(title: 'Flutterando 3'));
-}
-
-class AppWidget extends StatelessWidget {
-  final String title;
-
-  const AppWidget({Key? key, required this.title}) : super(key: key);
-
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      theme: ThemeData(primarySwatch: Colors.red),
-      home: HomePage(),
-    );
-  }
-}
-
-class HomePage extends StatefulWidget {
-  State<HomePage> createState() {
-    return HomePageState();
-  }
-}
-
-class HomePageState extends State<HomePage> {
-  int counter = 0;
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      child: Center(
-          child: GestureDetector(
-        child: Text('Contador: $counter'),
-        onTap: () {
-          setState(() {
-            counter++;
-          });
-        },
-      )),
-    );
-  }
+  runApp(AppWidget());
 }
