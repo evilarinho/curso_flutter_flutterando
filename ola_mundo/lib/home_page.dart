4e6af6b15a483e59059af33d93108466823f9484
edilson@Acer-E5-473 <ed.vilarinho@gmail.com>
2021-12-28 22:52:00 -0300

    atualizado até a aula12

--- ola_mundo/lib/home_page.dart
+++ ola_mundo/lib/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:ola_mundo/app_controller.dart';
 
 class HomePage extends StatefulWidget {
   State<HomePage> createState() {
@@ -8,23 +9,19 @@ class HomePage extends StatefulWidget {
 
 class HomePageState extends State<HomePage> {
   int counter = 0;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('HomePage'),
       ),
-      body: Container(
-        height: 200,
-        width: 200,
-        color: Colors.black,
-        child: Center(
-          child: Container(
-            height: 100,
-            width: 100,
-            color: Colors.green,
-          ),
-        ),
+      body: Center(
+        child: Switch(
+            value: AppController.instance.isDartTheme,
+            onChanged: (value) {
+              AppController.instance.changeTheme();
+            }),
       ),
       floatingActionButton: FloatingActionButton(
         child: Icon(Icons.add),
